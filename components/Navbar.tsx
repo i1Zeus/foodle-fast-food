811114ce6f82611d2f196f0f9c7078442f0092d7
Hussein Najah <husseinnajah123@gmail.com>
2024-11-16 21:51:39 +0300

    refactor Navbar component: enhance layout, add responsive menu, and integrate CommandSearch

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,37 +1,78 @@
-import { Button } from "./ui/button";
+"use client";
 
-const Navbar = () => {
+import { cn } from "@/lib/utils";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { CommandSearch } from "./CommandSearch";
+
+export default function Navbar() {
+  const [state, setState] = useState(false);
+  const pathname = usePathname();
+  const active = pathname;
+
+  const menus = [
+    { title: "Home", path: "/" },
+    { title: "Menu", path: "/menu" },
+    { title: "About Us", path: "/about" },
+  ];
+
   return (
-    <nav className="px-9 flex items-center justify-between w-full p-2 bg-transparent">
-      {/* Name */}
-      <h1 className="text-2xl font-extrabold">Foodle</h1>
-
-      {/* Links */}
-      <ul className="flex space-x-4">
-        <li>
-          <a href="/">
-            <Button variant="link">Home</Button>
-          </a>
-        </li>
-        <li>
-          <a href="/menu">
-            <Button variant="link">Menu</Button>
-          </a>
-        </li>
-        <li>
-          <a href="/about">
-            <Button variant="link">About Us</Button>
-          </a>
-        </li>
-      </ul>
-
-      {/* Auth */}
-      <div>
-        <Button variant="ghost">Login</Button>
-        <Button variant="default">Sign Up</Button>
+    <nav className="md:border-0 w-full bg-white border-b">
+      <div className="md:flex max-w-screen-2xl items-center px-4 mx-auto">
+        <div className="md:py-5 md:block flex items-center justify-between py-3">
+          <Link href="/">
+            <h1 className="text-3xl font-bold text-black">Foodle</h1>
+          </Link>
+          <div className="md:hidden">
+            <button
+              className="focus:border-gray-400 focus:border p-2 text-gray-700 rounded-md outline-none"
+              onClick={() => setState(!state)}
+            >
+              <Menu />
+            </button>
+          </div>
+        </div>
+        <div
+          className={cn(
+            "flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0",
+            state ? "block" : "hidden"
+          )}
+        >
+          <ul className="md:flex md:space-x-6 md:space-y-0 items-center justify-center space-y-8">
+            {menus.map((item, id) => (
+              <li
+                key={id}
+                className={cn(
+                  "hover:text-orange-600 text-gray-600 font-bold transition-all duration-150",
+                  active === item.path ? "text-orange-600" : ""
+                )}
+              >
+                <Link href={item.path}>{item.title}</Link>
+                <span></span>
+              </li>
+            ))}
+            <form className="relative flex items-center p-2 space-x-2 rounded-md">
+              <CommandSearch />
+            </form>
+          </ul>
+        </div>
+        <div className="md:flex hidden gap-3">
+          <Link
+            href="/login"
+            className="hover:bg-orange-500 hover:text-white px-4 py-2 text-orange-500 transition-all duration-150 rounded-full"
+          >
+            Login
+          </Link>
+          <Link
+            href="/sign-up"
+            className="hover:bg-orange-500 hover:text-white px-4 py-2 text-orange-500 transition-all duration-150 border-2 border-orange-500 rounded-full"
+          >
+            Sign Up
+          </Link>
+        </div>
       </div>
     </nav>
   );
-};
-
-export default Navbar;
+}
