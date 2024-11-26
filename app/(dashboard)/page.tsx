86bf4868b4f4e1e0f5d565d119b2019926566f9c
Hussein Najah <husseinnajah123@gmail.com>
2024-11-26 20:05:02 +0300

    add sections to dashboard: include BestDelivered, RegularMenu, and Ads components

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,15 @@
-import Hero from "@/components/Hero";
+import Hero from "@/components/Hero/Hero";
+import BestDelivered from "@/components/Sections/One/BestDelivered";
+import Ads from "@/components/Sections/Three/Ads";
+import RegularMenu from "@/components/Sections/Two/RegularMenu";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
+      <BestDelivered />
+      <RegularMenu />
+      <Ads />
     </main>
   );
 }
